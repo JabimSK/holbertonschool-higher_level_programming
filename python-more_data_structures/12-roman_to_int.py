7a45136f2e4bc4d0928cfa692aa3213b82d1d0af
Josue <joshermo15@gmail.com>
2023-06-08 12:12:21 -0700

    12-roman_to_int.py

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,15 +1,13 @@
-
 #!/usr/bin/python3
 def to_subtract(list_num):
     to_sub = 0
     max_list = max(list_num)
-    
+
     for n in list_num:
         if max_list > n:
             to_sub += n
 
     return (max_list - to_sub)
-
 
 def roman_to_int(roman_string):
     if not roman_string:
@@ -25,7 +23,7 @@ def roman_to_int(roman_string):
     last_rom = 0
     list_num = [0]
 
-    for ch in roman_string
+    for ch in roman_string:
         for r_num in list_keys:
             if r_num == ch:
                 if rom_n.get(ch) <= last_rom:
