8a38f7832c424ec2cd7a965fa25886f547d9d258
Josue <joshermo15@gmail.com>
2023-06-18 21:23:03 -0700

    2-matrix_divided.py

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -1,39 +1,30 @@
 #!/usr/bin/python3
 """
-Module Matrix_divided
-Divided matrix
+This is the "2-matrix_divided" module.
+The 2-matrix_divided module supplies one function, matrix_divided(matrix, div).
 """
 
 
 def matrix_divided(matrix, div):
-    """Returns a matrix
-    of results of a divided matrix
-    """
-    if type(matrix) != list or len(matrix) == 0 or matrix[0] is None:
-        raise TypeError("Matrix must be a matrix (list of lists) of \
+    """Divides all elements in the matrix by div"""
+    if type(matrix) is not list:
+        raise TypeError(
+            "matrix must be a matrix (list of lists) of integers/floats")
+    size = None
+    for l in matrix:
+        if type(l) is not list:
+            raise TypeError(
+                "matrix must be a matrix (list of lists) of integers/floats")
+        if size is None:
+            size = len(l)
+        elif size != len(l):
+            raise TypeError("Each row of the matrix must have the same size")
+        for i in l:
+            if type(i) is not int and type(i) is not float:
+                raise TypeError("matrix must be a matrix (list of lists) of \
 integers/floats")
-
-    for r in matrix:
-        if len(r) == 0:
-            raise TypeError("Matrix must be a matrix (list of lists) of \
-integers/floats")
-        for i in r:
-            if type(i) != int and type(i) != float:
-                raise TypeError("Matrix must be a matrix (list of lists) of \
-integers/floats")
-
-    lr = []
-    for r in matrix:
-        lr.append(len(r))
-    if not all(item == lr[0] for item in lr):
-        raise TypeError("Each row of the matrix must have the same size")
-
-    if type(div) != int and type(div) != float:
+    if type(div) is not int and type(div) is not float:
         raise TypeError("div must be a number")
-
     if div == 0:
         raise ZeroDivisionError("division by zero")
-
-    nm = [[round(x / div, 2) for x in r] for r in matrix]
-
-    return nm
+    return [[round(i / div, 2) for i in l] for l in matrix]
